86d40f02a0655b911e66e35e9683baf93c4f8875
alexx2203 <148546587+alexx2203@users.noreply.github.com>
2025-04-10 10:47:13 +0200

    Add files via upload

---
+++ AD Leichenbeseitigung.ps1
@@ -0,0 +1,18 @@
+﻿cls 
+$notactive = "Nie angemeldet"
+$user = @(Get-ADUser -Filter * -Properties Surname, GivenName, lastLogonTimestamp | 
+    Where-Object {
+    $_.GivenName -ne $null -and 
+    $_.Surname -ne $null } |
+    Select-Object -Property Surname, GivenName, lastLogonTimestamp) 
+    
+    
+
+#foreach($entry in $user){
+#if ($entry.lastLogonTimestamp -ne $null){
+#Write-Host $($entry.Surname) $($entry.GivenName) $([DateTime]::FromFileTime($entry.lastLogonTimestamp))}
+#else {Write-Host $($entry.Surname) $($entry.GivenName) $notactive
+#}}
+
+
+$user | Format-Table Surname,  Givenname, lastLogonTimestamp
